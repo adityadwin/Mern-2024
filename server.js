e523ee0b823f1cab92a40750d6c6f09debcbfd49
Aditya DwiN <131797940+adityadwin@users.noreply.github.com>
2024-05-30 23:40:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/public/index.html"))
+);
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
@@ -33,10 +37,6 @@ app.use(express.static(path.join(__dirname, "client/public")));
 
 // static assets
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/client/public/index.html"))
-);
 
 app.use(invalidPathHandler);
 app.use(errorResponserHandler);
