87b673bc23d652a5b2ae36126e8b2cfbe36c91b3
Aditya DwiN <131797940+adityadwin@users.noreply.github.com>
2024-05-30 23:35:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
